adc5dd3bedfff7795c67e221b70bf5c2de2679b6
Asma <asma.hussaini@zartis.com>
2022-10-13 11:27:01 +0100

    print result

--- Temperatures.js
+++ Temperatures.js
@@ -15,6 +15,16 @@ else{
         result = t;
         }
     }
-
-
-
+for (let i = 1; i < n; i++) {
+    const t = parseInt(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
+    if (Math.abs(result) > Math.abs(t))
+    
+     {
+        result = t;
+    }
+    else if (Math.abs(result) === Math.abs(t)){
+    }
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+console.log(result);
+}
