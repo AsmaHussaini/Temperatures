d2d3f85e34c12474f75ee7d58f15fcce959647cc
Asma <asma.hussaini@zartis.com>
2022-10-13 11:07:44 +0100

    initialize n (number of temperatures to analyse

---
+++ Temperatures.js
@@ -0,0 +1,8 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+ const n = parseInt(readline()); // the number of temperatures to analyse
+var inputs = readline().split(' ');
+var result;
